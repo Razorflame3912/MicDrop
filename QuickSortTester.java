ee7209250adbd5c4a1fc3f853460cccab9d10222
ilam1 <ilam1@stuy.edu>
2017-03-09 22:06:09 -0500

    Updated with actual nanotimes

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -14,12 +14,12 @@
 
   Mean execution times for dataset of size n:
   Batch size: 1000
-  n=1       time: 0.0 ns
-  n=10      time: 0.0 ns
-  n=100     time: 0.003 ns
-  n=1000    time: 0.048 ns 
-  n=10000   time: 0.522 ns
-  n=100000  time: 7.525 ns
+  n=1       time: 5.733 ns
+  n=10      time: 506.055 ns
+  n=100     time: 8322.598 ns
+  n=1000    time: 105081.703 ns 
+  n=10000   time: 1215392.038 ns
+  n=100000  time: 19174942.643 ns
   
   ANALYSIS:
   Based on the curve of the above data when graphed, the algorithm most closely resembles a run time of nlogn, hence reaffirming our speculations.
